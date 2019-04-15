6ca7e73ccc4cc37eba0e02009e06b103bdfe3395
Thomas E. Vaughan <tevaughan@gmail.com>
2019-04-14 22:23:09 -0600

    Added more tests.

---
+++ test/encoding-test.cpp
@@ -0,0 +1,24 @@
+// Copyright 2019  Thomas E. Vaughan
+// Redistributable according to GPL Version 3 or later.
+
+#include "../include/vnix/rat/encoding.hpp"
+#include "catch.hpp"
+
+using normalized_pair = vnix::rat::normalized_pair<uint8_t>;
+using encoding        = vnix::rat::encoding<uint8_t>;
+
+
+TEST_CASE("Limiting values are encoded and decoded.", "[encoding]") {
+  encoding const e1(normalized_pair(-16, 1));
+  encoding const e2(normalized_pair(+15, 1));
+  encoding const e3(normalized_pair(+1, 8));
+
+  REQUIRE(e1.n() == -16);
+  REQUIRE(e1.d() == 1);
+
+  REQUIRE(e2.n() == +15);
+  REQUIRE(e2.d() == 1);
+
+  REQUIRE(e3.n() == +1);
+  REQUIRE(e3.d() == 8);
+}
