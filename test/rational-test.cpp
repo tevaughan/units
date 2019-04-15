aceaad8f9311c1ca0045fc9c73f5154e5e3084c2
Vaughan, Thomas E <tevaugha@ball.com>
2019-04-15 13:23:37 -0600

    Added test of reciprocal.

--- test/rational-test.cpp
+++ test/rational-test.cpp
@@ -43,3 +43,11 @@ TEST_CASE("Addition and subtraction work.", "[rational]") {
   REQUIRE(r2 + r3 == rat16_t(-7, 12));
   REQUIRE(r2 - r3 == rat16_t(-11, 12));
 }
+
+TEST_CASE("Reciprocal works as expected.", "[rational]") {
+  rat8_t r1(-3, 2);
+  rat8_t r2(-2, 3);
+  REQUIRE(r1.reciprocal() == r2);
+  REQUIRE_NOTHROW(rat8_t(8).reciprocal());
+  REQUIRE_THROWS(rat8_t(9).reciprocal());
+}
