5f2d2b191dfc88f01f7eb0b5622044b9c1b2bfff
Vaughan, Thomas E <tevaugha@ball.com>
2019-04-16 09:54:37 -0600

    Added tests for statdim; moved stuff around.

--- include/vnix/rat/rational.hpp
+++ include/vnix/rat/rational.hpp
@@ -6,6 +6,8 @@
 #ifndef VNIX_RAT_RATIONAL_HPP
 #define VNIX_RAT_RATIONAL_HPP
 
+#include <iostream>
+#include <vnix/rat/common-denom.hpp>
 #include <vnix/rat/encoding.hpp>
 
 namespace vnix {
@@ -60,12 +62,22 @@ public:
   /// Modify this rational number by adding other rational number.
   /// @param r  Other rational number.
   /// @return   Reference to this rational number (after addition).
-  constexpr rational &operator+=(rational r) { return *this = *this + r; }
+  constexpr rational &operator+=(rational r);
+
+  /// Modify this rational number by adding other rational number.
+  /// @param r  Other rational number (in this case, an integer).
+  /// @return   Reference to this rational number (after addition).
+  constexpr rational &operator+=(int64_t r);
 
   /// Modify this rational number by subtracting other rational number.
   /// @param r  Other rational number.
   /// @return   Reference to this rational number (after subtraction).
-  constexpr rational &operator-=(rational r) { return *this = *this - r; }
+  constexpr rational &operator-=(rational r);
+
+  /// Modify this rational number by subtracting other rational number.
+  /// @param r  Other rational number (in this case, an integer).
+  /// @return   Reference to this rational number (after subtraction).
+  constexpr rational &operator-=(int64_t r);
 
   /// Reciprocal of this rational number.
   constexpr rational reciprocal() const {
@@ -81,12 +93,22 @@ public:
   /// Modify this rational number by multiplying by other rational number.
   /// @param r  Other rational number.
   /// @return   Reference to this rational number (after multiplication).
-  constexpr rational &operator*=(rational r) { return *this = (*this) * r; }
+  constexpr rational &operator*=(rational r);
 
   /// Modify this rational number by dividing by other rational number.
   /// @param r  Other rational number.
   /// @return   Reference to this rational number (after division).
-  constexpr rational &operator/=(rational r) { return *this = (*this) / r; }
+  constexpr rational &operator/=(rational r);
+
+  /// Modify this rational number by multiplying by other rational number.
+  /// @param r  Other rational number (in this case, integer).
+  /// @return   Reference to this rational number (after multiplication).
+  constexpr rational &operator*=(int64_t r);
+
+  /// Modify this rational number by dividing by other rational number.
+  /// @param r  Other rational number (in this case, integer).
+  /// @return   Reference to this rational number (after division).
+  constexpr rational &operator/=(int64_t r);
 
   /// Encoding from rational number.
   constexpr static U encode(rational r) { return r.c_; }
@@ -94,6 +116,400 @@ public:
   /// Rational number from encoding.
   constexpr static rational decode(U u) { return {u, dummy_arg()}; }
 };
+
+
+/// Compare rationals for equality.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator==(rational<uint64_t> r1, rational<uint64_t> r2) {
+  return r1.n() == r2.n() && r1.d() == r2.d();
+}
+
+
+/// Compare rationals for equality.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator==(int64_t r1, rational<uint64_t> r2) {
+  return rational<uint64_t>(r1) == r2;
+}
+
+
+/// Compare rationals for equality.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator==(rational<uint64_t> r1, int64_t r2) {
+  return r1 == rational<uint64_t>(r2);
+}
+
+
+/// Compare rationals for inequality.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator!=(rational<uint64_t> r1, rational<uint64_t> r2) {
+  return !(r1 == r2);
+}
+
+
+/// Compare rationals for inequality.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator!=(int64_t r1, rational<uint64_t> r2) {
+  return !(r1 == r2);
+}
+
+
+/// Compare rationals for inequality.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator!=(rational<uint64_t> r1, int64_t r2) {
+  return !(r1 == r2);
+}
+
+
+/// Compare for less-than-or-equal ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator<=(rational<uint64_t> r1, rational<uint64_t> r2) {
+  common_denom const c(r1, r2);
+  return c.n1 <= c.n2;
+}
+
+
+/// Compare for less-than-or-equal ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator<=(int64_t r1, rational<uint64_t> r2) {
+  return rational<uint64_t>(r1) <= r2;
+}
+
+
+/// Compare for less-than-or-equal ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator<=(rational<uint64_t> r1, int64_t r2) {
+  return r1 <= rational<uint64_t>(r2);
+}
+
+
+/// Compare for less-than ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator<(rational<uint64_t> r1, rational<uint64_t> r2) {
+  rat::common_denom const c(r1, r2);
+  return c.n1 < c.n2;
+}
+
+
+/// Compare for less-than ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator<(int64_t r1, rational<uint64_t> r2) {
+  return rational<uint64_t>(r1) < r2;
+}
+
+
+/// Compare for less-than ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator<(rational<uint64_t> r1, int64_t r2) {
+  return r1 < rational<uint64_t>(r2);
+}
+
+
+/// Compare for greater-than-or-equal ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator>=(rational<uint64_t> r1, rational<uint64_t> r2) {
+  common_denom const c(r1, r2);
+  return c.n1 >= c.n2;
+}
+
+
+/// Compare for greater-than-or-equal ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator>=(int64_t r1, rational<uint64_t> r2) {
+  return rational<uint64_t>(r1) >= r2;
+}
+
+
+/// Compare for greater-than-or-equal ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator>=(rational<uint64_t> r1, int64_t r2) {
+  return r1 >= rational<uint64_t>(r2);
+}
+
+
+/// Compare for greater-than ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator>(rational<uint64_t> r1, rational<uint64_t> r2) {
+  rat::common_denom const c(r1, r2);
+  return c.n1 > c.n2;
+}
+
+
+/// Compare for greater-than ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator>(int64_t r1, rational<uint64_t> r2) {
+  return rational<uint64_t>(r1) > r2;
+}
+
+
+/// Compare for greater-than ordering with another rational.
+/// Promote both to uint64_t-storage for comparison.
+/// @param r1  Left -hand operand.
+/// @param r2  Right-hand operand.
+constexpr bool operator>(rational<uint64_t> r1, int64_t r2) {
+  return r1 > rational<uint64_t>(r2);
+}
+
+
+/// Copy rational number by way of unary operator+.
+/// @tparam U  Type of unsigned word in which rational is encoded.
+/// @param  r  Number to copy.
+/// @return    Copy of number.
+template <typename U> constexpr rational<U> operator+(rational<U> r) {
+  return r;
+}
+
+
+/// Negate rational number.
+/// @tparam U  Type of unsigned word in which rational is encoded.
+/// @param  r  Number to negate.
+/// @return    Negative of number.
+template <typename U> constexpr rational<U> operator-(rational<U> r) {
+  return {-r.n(), r.d()};
+}
+
+
+/// Sum of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Addend.
+/// @param  r2  Adder.
+/// @return     Sum.
+template <typename U>
+constexpr rational<U> operator+(rational<U> r1, rational<U> r2) {
+  rat::common_denom const c(r1, r2);
+  return {c.n1 + c.n2, c.d};
+}
+
+
+/// Sum of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Addend.
+/// @param  r2  Adder.
+/// @return     Sum.
+template <typename U>
+constexpr rational<U> operator+(int64_t r1, rational<U> r2) {
+  return rational<U>(r1) + r2;
+}
+
+
+/// Sum of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Addend.
+/// @param  r2  Adder.
+/// @return     Sum.
+template <typename U>
+constexpr rational<U> operator+(rational<U> r1, int64_t r2) {
+  return r1 + rational<U>(r2);
+}
+
+
+// Modify rational number by adding other rational number.
+template <typename U>
+constexpr rational<U> &rational<U>::operator+=(rational<U> r) {
+  return *this = *this + r;
+}
+
+
+// Modify rational number by adding other rational number.
+template <typename U>
+constexpr rational<U> &rational<U>::operator+=(int64_t r) {
+  return *this = *this + r;
+}
+
+
+/// Difference between two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Minuend.
+/// @param  r2  Subtrahend.
+/// @return     Difference.
+template <typename U>
+constexpr rational<U> operator-(rational<U> r1, rational<U> r2) {
+  return -r2 + r1;
+}
+
+
+/// Difference between two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Minuend.
+/// @param  r2  Subtrahend.
+/// @return     Difference.
+template <typename U>
+constexpr rational<U> operator-(int64_t r1, rational<U> r2) {
+  return -r2 + r1;
+}
+
+
+/// Difference between two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Minuend.
+/// @param  r2  Subtrahend.
+/// @return     Difference.
+template <typename U>
+constexpr rational<U> operator-(rational<U> r1, int64_t r2) {
+  return -r2 + r1;
+}
+
+
+// Modify this rational number by subtracting other rational number.
+template <typename U>
+constexpr rational<U> &rational<U>::operator-=(rational<U> r) {
+  return *this = *this - r;
+}
+
+
+// Modify this rational number by subtracting other rational number.
+template <typename U>
+constexpr rational<U> &rational<U>::operator-=(int64_t r) {
+  return *this = *this - r;
+}
+
+
+/// Product of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Multiplicand.
+/// @param  r2  Multiplier.
+/// @return     Product.
+template <typename U>
+constexpr rational<U> operator*(rational<U> r1, rational<U> r2) {
+  int64_t const  n1 = r1.n();
+  int64_t const  n2 = r2.n();
+  uint64_t const d1 = r1.d();
+  uint64_t const d2 = r2.d();
+  uint64_t const ga = gcd((n1 < 0 ? -n1 : n1), d2);
+  uint64_t const gb = gcd((n2 < 0 ? -n2 : n2), d1);
+  return {int64_t(n1 / ga * n2 / gb), int64_t(d1 / gb * d2 / ga)};
+}
+
+
+/// Product of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Multiplicand.
+/// @param  r2  Multiplier.
+/// @return     Product.
+template <typename U>
+constexpr rational<U> operator*(int64_t r1, rational<U> r2) {
+  return rational<U>(r1) * r2;
+}
+
+
+/// Product of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Multiplicand.
+/// @param  r2  Multiplier.
+/// @return     Product.
+template <typename U>
+constexpr rational<U> operator*(rational<U> r1, int64_t r2) {
+  return r1 * rational<U>(r2);
+}
+
+
+/// Quotient of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Dividend.
+/// @param  r2  Divisor.
+/// @return     Quotient.
+template <typename U>
+constexpr rational<U> operator/(rational<U> r1, rational<U> r2) {
+  return r1 * r2.reciprocal();
+}
+
+
+/// Quotient of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Dividend.
+/// @param  r2  Divisor.
+/// @return     Quotient.
+template <typename U>
+constexpr rational<U> operator/(int64_t r1, rational<U> r2) {
+  return r1 * r2.reciprocal();
+}
+
+
+/// Quotient of two rational numbers.
+/// @tparam U   Type of unsigned word in which rational is encoded.
+/// @param  r1  Dividend.
+/// @param  r2  Divisor.
+/// @return     Quotient.
+template <typename U>
+constexpr rational<U> operator/(rational<U> r1, int64_t r2) {
+  return r1 * rational<U>(r2).reciprocal();
+}
+
+
+// Modify this rational number by multiplying by other rational number.
+template <typename U>
+constexpr rational<U> &rational<U>::operator*=(rational<U> r) {
+  return *this = (*this) * r;
+}
+
+
+// Modify this rational number by dividing by other rational number.
+template <typename U>
+constexpr rational<U> &rational<U>::operator/=(rational<U> r) {
+  return *this = (*this) / r;
+}
+
+
+// Modify this rational number by multiplying by other rational number.
+template <typename U>
+constexpr rational<U> &rational<U>::operator*=(int64_t r) {
+  return *this = (*this) * r;
+}
+
+
+// Modify this rational number by dividing by other rational number.
+template <typename U>
+constexpr rational<U> &rational<U>::operator/=(int64_t r) {
+  return *this = (*this) / r;
+}
+
+
+/// Print rational number to output-stream.
+/// @tparam U  Type of unsigned word in which rational is encoded.
+/// @param  s  Reference to output-stream.
+/// @param  r  Rational number.
+/// @return    Reference to modified output-stream.
+template <typename U>
+std::ostream &operator<<(std::ostream &s, rational<U> r) {
+  s << int64_t(r.n());
+  if (r.d() != 1) {
+    s << '/' << uint64_t(r.d());
+  }
+  return s;
+}
 
 
 } // namespace rat
--- test/statdim-base-test.cpp
+++ test/statdim-base-test.cpp
@@ -3,10 +3,51 @@
 
 using dim                                = vnix::units::dim;
 template <uint64_t D> using statdim_base = vnix::units::statdim_base<D>;
+using dyndim_base                        = vnix::units::dyndim_base;
+
 
 TEST_CASE("Check dimension on initialization from dim.", "[statdim-base]") {
   dim constexpr d({-1, 1, 0, 0, 0});
   dim e({1, 0, 0, 0, 0});
   REQUIRE_NOTHROW(statdim_base<d>(d));
   REQUIRE_THROWS(statdim_base<d>(e));
+}
+
+
+TEST_CASE("Sum & diff require same dimension.", "[statdim-base]") {
+  dim constexpr d1({-1, 1, 1, 0, 0});
+  dim constexpr d2({+1, 0, 0, 0, 0});
+
+  statdim_base<d1> sdb;
+  dyndim_base      ddb1(d1), ddb2(d2);
+
+  REQUIRE_NOTHROW(sdb.sum(sdb));
+  REQUIRE_NOTHROW(sdb.sum(ddb1));
+  REQUIRE_NOTHROW(sdb.diff(sdb));
+  REQUIRE_NOTHROW(sdb.diff(ddb1));
+
+  REQUIRE(sdb.sum(sdb).d() == d1);
+  REQUIRE(sdb.sum(ddb1).d() == d1);
+  REQUIRE(sdb.diff(sdb).d() == d1);
+  REQUIRE(sdb.diff(ddb1).d() == d1);
+
+  REQUIRE_THROWS(sdb.sum(ddb2));
+  REQUIRE_THROWS(sdb.diff(ddb2));
+}
+
+
+TEST_CASE("Prod & quot change dimension appropriately.", "[statdim-base]") {
+  dim constexpr d1({-1, 1, 1, 0, 0});
+  dim constexpr d2({+1, 0, 0, 0, 0});
+  dim constexpr d3({+0, 1, 1, 0, 0});
+  dim constexpr d4({-2, 1, 1, 0, 0});
+
+  statdim_base<d1> sdb1;
+  statdim_base<d2> sdb2;
+  dyndim_base      ddb2(d2);
+
+  REQUIRE(sdb1.prod(sdb2).d() == d3);
+  REQUIRE(sdb1.quot(sdb2).d() == d4);
+  REQUIRE(sdb1.prod(ddb2).d() == d3);
+  REQUIRE(sdb1.quot(ddb2).d() == d4);
 }
