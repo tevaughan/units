ff4ebeb0a9d90006655062a33f9f45c9ed6a0730
Thomas E. Vaughan <tevaughan@gmail.com>
2019-05-06 14:46:25 -0600

    Attempt to improve coverage.

--- test/rational-test.cpp
+++ test/rational-test.cpp
@@ -14,19 +14,26 @@ TEST_CASE("Constructor from num and den works as expected.", "[rational]") {
   rat8_t constexpr r1;
   REQUIRE(!r1.to_bool());
   REQUIRE(r1.to_int() == 0);
+  REQUIRE(r1.to_float() == 0);
+  REQUIRE(r1.to_double() == 0);
 
   rat8_t constexpr r2(3);
   REQUIRE(r2.to_bool());
   REQUIRE(r2.to_int() == 3);
+  REQUIRE(r2.to_float() == 3);
+  REQUIRE(r2.to_double() == 3);
 
   rat8_t constexpr r3(3, 2);
   REQUIRE(r3.to_bool());
   REQUIRE_THROWS(r3.to_int());
+  REQUIRE(r3.to_float() == 1.5);
   REQUIRE(r3.to_double() == 1.5);
 
   rat8_t constexpr r4(4, -4);
   REQUIRE(r4.to_bool());
   REQUIRE(r4.to_int() == -1);
+  REQUIRE(r4.to_float() == -1);
+  REQUIRE(r4.to_double() == -1);
 }
 
 
@@ -39,10 +46,12 @@ TEST_CASE("Conversion-constructor works as expected.", "[rational]") {
 
 
 TEST_CASE("Addition and subtraction work.", "[rational]") {
-  rat8_t r1(3, 2);
-  r1 += rat16_t(1);
+  rat8_t  r1(3, 2);
+  rat16_t r1a(1);
+  r1 += r1a;
   REQUIRE(r1 == rat8_t(5, 2));
-  r1 -= rat32_t(1);
+  rat32_t r1s(1);
+  r1 -= r1s;
   REQUIRE(r1 == rat8_t(3, 2));
   rat16_t constexpr r2(-3, 4);
   rat16_t constexpr r3(1, 6);
@@ -65,9 +74,11 @@ TEST_CASE("Multiplication and division work.", "[rational]") {
   rat8_t r2(-1, 4);
   REQUIRE(r1 * r2 == rat8_t(3, 8));
   REQUIRE(r1 / r2 == rat8_t(6));
-  r1 *= rat8_t(1, 2);
+  rat8_t r1m(1, 2);
+  r1 *= r1m;
   REQUIRE(r1 == rat8_t(-3, 4));
-  r1 /= rat8_t(2, 3);
+  rat8_t r1d(2, 3);
+  r1 /= r1d;
   REQUIRE(r1 == rat8_t(-9, 8));
 }
 
